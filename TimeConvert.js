573faf9c37c873bf1a05a7df1096577e7fbaecab
Amira Hailemariam <amira.dhaile@gmail.com>
2015-02-10 16:30:14 -0800

    Delete TimeConvert.js

--- TimeConvert.js
+++
@@ -1,5 +0,0 @@
-function TimeConvert(num) {
-  var hrs = Math.floor(num / 60); // Divides by 60 minutes and rounds down to whole hour
-  var mins = num % 60; // Finds remainder minutes 
-  return hrs + ":" + mins; // Formats to hrs:mins
-}
