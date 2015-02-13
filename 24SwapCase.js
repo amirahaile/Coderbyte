03862b2d52ff34cd74d5319b380efc567e62eaaf
amirahaile <amira.dhaile@gmail.com>
2015-02-13 11:14:15 -0800

    init solution

---
+++ 24SwapCase.js
@@ -0,0 +1,13 @@
+function SwapCase(str) {
+  var array = str.split(''); // turns the String into an Array
+  
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === array[i].toLowerCase()) { // checks if the character is lowercase
+      array[i] = array[i].toUpperCase(); // if it is, transforms it to uppercase
+    } else {
+      array[i] = array[i].toLowerCase(); // if it isn't, transforms it to lowercase
+    }
+  }
+
+  return array.join().replace(/,/g, ''); // turns Array into String and removes commas
+}
